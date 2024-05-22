52a8c1916c75443b65adb42a416f8289338a02db
Bryce Belen <bryce.belen@gmail.com>
2024-05-21 19:56:55 -0700

    fix port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
